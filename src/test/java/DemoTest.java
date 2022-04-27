86484503f452a521ea381fc88c9d2f4a1d46b04d
MdShakil14 <mdshakil1420414@gmail.com>
2022-04-28 01:53:40 +0600

    Print log report in file and configure log4j2.xml file.

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,13 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class DemoTest {
+
+    private static Logger log = LogManager.getLogger(DemoTest.class.getName());
+    public static void main(String[] args) {
+
+        log.info("Sjakil");
+        log.error("Shaki not here");
+
+    }
+}
